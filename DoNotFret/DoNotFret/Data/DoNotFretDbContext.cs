d64cdd504c7824757fa9376b582a28f65b22bd92
Jona Brown <66106310+SorviusN@users.noreply.github.com>
2021-08-16 23:22:27 -0700

    Stuff

---
+++ DoNotFret/DoNotFret/Data/DoNotFretDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using DoNotFret.Models;
+
+namespace DoNotFret.Data
+{
+    public class DoNotFretDbContext : DbContext 
+    {
+        public DbSet<StringInstrument> StringInstrument { get; set; }
+        public DbSet<WindInstrument> WindInstrument { get; set; }
+
+        //constructor
+        public DoNotFretDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
