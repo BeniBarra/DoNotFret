6f2ea1c5b3f140a3c29875e570d78581351d777a
Benjamin <loco.queens@gmail.com>
2021-08-17 20:57:38 -0700

    Lab 27 complete, pushing for submission

---
+++ DoNotFret/DoNotFret/Services/I_Instruments.cs
@@ -0,0 +1,21 @@
+﻿using DoNotFret.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DoNotFret.Services
+{
+    public interface I_Instruments
+    {
+        Task<Instruments> Create(Instruments instrument);
+
+        Task<List<Instruments>> GetAll();
+
+        Task<Instruments> GetOne(int id);
+
+        Task<Instruments> Update(Instruments instruments);
+
+        Task Delete(int id);
+    }
+}
